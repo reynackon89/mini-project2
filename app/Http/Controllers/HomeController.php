522b316a0bea89584c9857245ab269dbdc564804
Reyn Ackon <reynackon@gmail.com>
2018-04-22 20:46:54 -0400

    Homepage 1 - stuff

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,7 +25,7 @@ class HomeController extends Controller
     public function index()
     {
         $user = Auth::user();
-        $questions = $user->questions()->paginate(10);
+        $questions = $user->questions()->paginate(6);
         return view('home')->with('questions', $questions);
     }
 }
