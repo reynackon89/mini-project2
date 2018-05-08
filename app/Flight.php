70605647c5b49b3ddf229e27030ac0a6c7c35b2d
Reyn Ackon <reynackon@gmail.com>
2018-05-07 23:31:31 -0400

    Add votes to users

--- app/Flight.php
+++ app/Flight.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Flight extends Model
 {
-    //
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'my_flights';
 }
